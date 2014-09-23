bdda14e6ddcc3d3bc0fc86aed167fa3e5d4984bf
tmarcon <tehem@tehem.net>
2014-09-23 17:28:12 +0200

    composer install for projects

--- setup/install_projects.sh
+++ setup/install_projects.sh
@@ -5,6 +5,7 @@ normal=`tput sgr0`
 
 # Getting the project id (standard SVN one)
 ENV=$( echo "$1" | sed 's,/,,g' )
+
 if [ "$ENV" = "" ]; then
     echo "Please provide a configuration environment name, available environments: "
     ls ./config/*.conf | sed 's/.*\/\(.*\).conf/\1/'
@@ -145,6 +146,7 @@ function initialize_project_sources_svn() {
 }
 
 function initialize_project_configuration() {
+	PWD=$( pwd )	
     project="$1"
     
     echo "==> Adding host record for project virtual host ${bold}${project}.${VIRTUAL_DOMAIN}${normal}..."
@@ -197,8 +199,8 @@ function initialize_project_configuration() {
         # Look for database dumps if any
         count=`ls -1 $src/$PROJECT_PATH_DUMPS/*.sql 2>/dev/null | wc -l`
 
-        if [ $count != 0 ]
-        then              
+        if [ $count != 0 ]; then
+		
             # Database is required:
             databases=$( ls $src/$PROJECT_PATH_DUMPS/*.sql | sed 's/.*\///' )
             for database in $databases; do
@@ -220,6 +222,13 @@ function initialize_project_configuration() {
                 $sql ${db} < "$src/$PROJECT_PATH_DUMPS/$database"
             done
         fi
+		
+        # If a composer.json is present in the configuration
+        if [ -f "$src/composer.json" ]; then
+            cd "$src"
+            composer install            
+            cd "${PWD}"
+        fi		
     fi
 }
 
